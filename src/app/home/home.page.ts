e2557bf9c74ec7ff0d4edc28a8460af8fb0df3c0
Fabio-Frare <44546034+Fabio-Frare@users.noreply.github.com>
2025-03-29 16:32:15 -0300

    Implementação completa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  resultado: string = ""
+  precoEtanol: any;
+  precoGasolina: any;
+
   constructor() {}
+
+  calcular() {
+    if(this.precoEtanol && this.precoGasolina) {
+      var pEtanol   = parseFloat(this.precoEtanol)
+      var pGasolina = parseFloat(this.precoGasolina)
+      var res       = pEtanol / pGasolina
+      if(res >= 0.7) {
+        this.resultado = "Melhor utilizar gasolina."
+      } else {
+        this.resultado = "Melhor utilizar etanol."
+      }
+    } else {
+      this.resultado = "Informe os preços."
+    }
+  }
 
 }
